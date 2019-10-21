b6506a1f97fcb959cc28aeb97bd87c8e3a6de0c6
Olgerdas <55126440+Olgerdas@users.noreply.github.com>
2019-10-21 15:19:16 +0300

    Klases Darbas Nr.5

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+$array = [
+    'takas_pirmas' => [
+        'parengimas' => 'svarus',
+        'kegliai' => 5,
+        'kamoliukai' => 4,
+    ],
+           'takas_antras' => [
+        'parengimas' => 'nesvarus',
+    'kegliai' => 0,
+    'kamoliukai' => 0,
+    ],
+        ];
+        var_dump($array);      
+?>
+<html>
+   
+    <head>     <title></title>
+    </head>
+    <body>
+                
+    </body>        
+</html>
+
+
+
+
+
+
+
+        
+        
+            
+       
+          
+ 
